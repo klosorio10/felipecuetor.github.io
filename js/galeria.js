0bc7344559195d2ffa34bb735fa768bec2116840
Felipe Cueto <f.cueto10@uniandes.edu.co>
2017-02-15 01:29:33 -0500

    Fin

---
+++ js/galeria.js
@@ -0,0 +1,30 @@
+var foto = 1;
+
+function cambioFoto(entrada) {
+	foto = entrada;
+	document.getElementById("galeria").setAttribute("src",foto+".jpg");
+	
+}
+
+function fotoMenosUno(entrada) {
+	foto = foto-1;
+		if(foto<0)
+	{
+		foto = 3;
+	}
+
+	
+	document.getElementById("galeria").setAttribute("src",foto+".jpg");
+	
+}
+
+function fotoMasUno(entrada) {
+	foto = foto+1;
+		if(foto>3)
+	{
+		foto = 1;
+	}
+
+	document.getElementById("galeria").setAttribute("src",foto+".jpg");
+	
+}
